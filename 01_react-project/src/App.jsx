1969f76a9dfd3fb449eb01412f6254e33f4b562c
Eugene Makarikhin <contact@aboutslack.info>
2026-01-13 14:01:32 +0100

    Added 01_react-project basic components

--- 01_react-project/src/App.jsx
+++ 01_react-project/src/App.jsx
@@ -1,6 +1,37 @@
-import { useState } from 'react'
+import "./App.css"
+
+function Header({name, year}) {
+  return(
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables"
+]
+
+function Main({dishes}) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li style={{ listStyleType: "none" }}>{dish}</li>
+      ))}
+    </ul>
+  )
+}
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <Header name="Alex" year={new Date().getFullYear()}/>
+      <Main dishes={items}/>
+    </div>
+  );
 }
 
 export default App
