ed8ac0955c7050edde39dfcfc582975ad60e917e
Eugene Makarikhin <contact@aboutslack.info>
2026-01-14 12:44:20 +0100

    Updated 02_next-react-project montains and hotels pages

--- 01_linkedin_react_essential/02_next-react-project/src/app/hotels/page.js
+++ 01_linkedin_react_essential/02_next-react-project/src/app/hotels/page.js
@@ -1,9 +1,31 @@
-export default function Page() {
-    return (
-        <main>
-          <div>
-            <h1>Hotel Details</h1>
-          </div>
-        </main>
-    )
+async function getData(params) {
+  const rest = await fetch(
+    "https://snowtooth-hotel-api.fly.dev"
+  );
+  return rest.json();
+}
+
+function HotelBlock({ name, capacity }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{capacity}</p>
+    </div>
+  )
+}
+
+export default async function Page() {
+  const data = await getData()
+  return (
+    <main>
+      <div>
+        <h1>Hotel Details</h1>
+        <div>
+          {data.map((hotel) => (
+            <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity} />
+          ))}
+        </div>
+      </div>
+    </main>
+  )
 }
