6fb8a57cb2a65301888f3963de12f1afbe0c71d0
Eugene Makarikhin <contact@aboutslack.info>
2026-04-01 23:17:07 +0200

    02_modern_react/01-react-from-scratch Init commit

---
+++ 02_modern_react/01-react-from-scratch/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+    return <h1>This is React Script!</h1>
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />)
