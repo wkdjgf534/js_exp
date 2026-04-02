50f690f16f2755c78585ef3ae83d0c1cb64f66b7
Eugene Makarikhin <contact@aboutslack.info>
2026-04-02 09:53:54 +0200

    02_modern_react/01-react-from-scratch Added some libraries with webpack

--- 02_modern_react/01-react-from-scratch/index.js
+++ 02_modern_react/01-react-from-scratch/index.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-    return <h1>This is React Script!</h1>
+    return <h1>This is React Script!</h1>;
 }
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
-root.render(<App />)
+root.render(<App />);
