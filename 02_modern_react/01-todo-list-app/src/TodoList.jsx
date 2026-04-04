3fbb4338289f480f90a4c4390f6f986c5312775e
Eugene Makarikhin <contact@aboutslack.info>
2026-04-04 11:37:08 +0200

    02_modern_react/01-todo-lisp-app Init commit

---
+++ 02_modern_react/01-todo-list-app/src/TodoList.jsx
@@ -0,0 +1,11 @@
+export default function TodoList({ completedTodos, incompletedTodos }) {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <p>New todo form will be here..</p>
+      <h3>Completed:</h3>
+
+      <h3>Incomplete:</h3>
+    </div>
+  )
+}
