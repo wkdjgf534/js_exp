76e38a0cab3a830a7a3c95eacd254a39b4be76bf
Eugene Makarikhin <contact@aboutslack.info>
2026-04-06 23:10:21 +0200

    02_modern_react_todo_app Added react redux lib

--- 02_modern_react_todo_app/src/main.jsx
+++ 02_modern_react_todo_app/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { configureStore} from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 
+const store = configureStore({
+  reducer: {},
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
