5ae4d61a3886d9ff8d506a64f51b1ba7939baf79
Eugene Makarikhin <contact@aboutslack.info>
2026-04-11 09:53:26 +0200

    02_modern_react_todo_app Added styled-component

--- 02_modern_react_todo_app/src/TodoListItem.jsx
+++ 02_modern_react_todo_app/src/TodoListItem.jsx
@@ -1,18 +1,26 @@
 import { useDispatch } from "react-redux";
 import { markTodoAsCompleted } from "./todoSlice";
 import { deleteTodo } from './thunks';
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  ${props => props.important && 'background-color: yellow;' }
+  border-radius: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+  padding: 16px;
+`;
 
 export default function TodoListItem({ todo, onCompletedClicked, onDeleteClicked }) {
   const dispatch = useDispatch();
 
 
   return (
-    <div>
+    <CardContainer important={todo.text.endsWith('!')}>
       <h3>{todo.text}</h3>
       {todo.isCompleted && <p>Complete!</p>}
       {todo.isCompleted
         ? <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete Item</button>
         : <button onClick={() => dispatch(markTodoAsCompleted(todo.text))}>Mark as Completed</button>}
-    </div>
+    </CardContainer>
   );
 }
